948adcb564610973e16db86f63f1a1f047f9c763
Arqam2000 <arqamsarwar9@gmail.com>
2025-07-16 12:11:39 +0500

    Updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -20,8 +20,11 @@ const Slider = () => {
                     images.map((image, index) => (
                         <div className="each-slide-effect">
                             <div style={{ 'backgroundImage': `url(${image})` }}>
-                                <div className='lg:w-2/6 w-[290px]  flex flex-col gap-2 lg:ml-10 ml-2  bg-gray-300 opacity-65'>
-                                    <h1 className='lg:text-xl font-bold text-base'>Asthetic Excellence for Ageless Beauty</h1>
+                                <div className='mx-2 mb-2'>
+                                    <h1 className='lg:text-xl font-bold text-base text-[#ff0a64]'>Asthetic Excellence for Ageless Beauty</h1>
+                                </div>
+                                <div className='lg:w-2/6 w-[290px]  flex flex-col gap-2 lg:ml-10 ml-2  bg-gray-300 opacity-65 px-2'>
+                                    
                                     <p className='font-normal text-justify text-sm'><span className='italic font-medium text-base block'>Welcome to Lam Aesthetics Clinic</span> Your destination for safe, effective, and elegant facial rejuvenation treatments. Specializing in Botox, Dermal Fillers, and Skin Boosters, we help you look and feel your best.
 
                                     </p >
