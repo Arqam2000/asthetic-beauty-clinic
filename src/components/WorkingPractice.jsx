fbc353e8e10068559b261d5c60ed0763937241f4
Arqam2000 <arqamsarwar9@gmail.com>
2025-07-16 13:12:51 +0500

    Updated

--- src/components/WorkingPractice.jsx
+++ src/components/WorkingPractice.jsx
@@ -10,31 +10,31 @@ const WorkingPractice = () => {
                 <h1 className='text-3xl'>Our Working Practice</h1>
             </div>
             <div className='flex items-start justify-center flex-wrap sm:flex-nowrap gap-4'>
-                <div className=' md:w-[25%] w-[70%] sm:w-1/3 p-3 shadow-md shadow-black sm:h-[520px]'>
-                    <div>
-                        <img src={preTreatmentLogo} alt="" className='w-full' />
+                <div className=' md:w-[25%] w-[70%] sm:w-1/3 p-3 shadow-md shadow-black sm:h-[470px] md:h-[450px] lg:h-[380px]'>
+                    <div className='flex justify-center'>
+                        <img src={preTreatmentLogo} alt="" className='w-[170px]' />
                     </div>
                     <div>
                         <h3 className='font-semibold text-xl'>Pre-Treatment Session</h3>
-                        <p>Every treatment begins with a private consultation. Our specialists assess your concerns, examine your facial profile, and recommend the most suitable options for you</p>
+                        <p className='text-justify'>Every treatment begins with a private consultation. Our specialists assess your concerns, examine your facial profile, and recommend the most suitable options for you</p>
                     </div>
                 </div>
-                <div className=' md:w-[25%] w-[70%] sm:w-1/3 p-3 shadow-md shadow-black sm:h-[520px]'>
-                    <div>
-                        <img src={treatmentLogo} alt="" className='w-full' />
+                <div className=' md:w-[25%] w-[70%] sm:w-1/3 p-3 shadow-md shadow-black sm:h-[470px] md:h-[450px] lg:h-[380px]'>
+                    <div className='flex justify-center'>
+                        <img src={treatmentLogo} alt="" className='w-[170px]' />
                     </div>
                     <div>
                         <h3 className='font-semibold text-xl'>Treatment</h3>
-                        <p>After your consultation, we move on to the treatment.  We take great care to make sure everything is done safely and to the highest standard. We want you to leave feeling confident and refreshed.</p>
+                        <p className='text-justify'>After your consultation, we move on to the treatment.  We take great care to make sure everything is done safely and to the highest standard. We want you to leave feeling confident and refreshed.</p>
                     </div>
                 </div>
-                <div className=' md:w-[25%] w-[70%] sm:w-1/3 p-3 shadow-md shadow-black sm:h-[520px]'>
-                    <div>
-                        <img src={postTreatmentLogo} alt="" className='w-full' />
+                <div className=' md:w-[25%] w-[70%] sm:w-1/3 p-3 shadow-md shadow-black sm:h-[470px] md:h-[460px] lg:h-[380px]'>
+                    <div className='flex justify-center'>
+                        <img src={postTreatmentLogo} alt="" className='w-[170px]' />
                     </div>
                     <div>
                         <h3 className='font-semibold text-xl'>Post-Treatment Guidance</h3>
-                        <p>After your consultation, we move on to the treatment.  We take great care to make sure everything is done safely and to the highest standard. We want you to leave feeling confident and refreshed.</p>
+                        <p className='text-justify'>After your consultation, we move on to the treatment.  We take great care to make sure everything is done safely and to the highest standard. We want you to leave feeling confident and refreshed.</p>
                     </div>
                 </div>
             </div>
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ContactForm from '../components/ContactForm.jsx'
 
 const ContactUs = () => {
-  return (
-    <div className='flex bg-gray-400 min-h-lvh justify-center items-center px-4 gap-3'>
-        <div className='bg-orange-300 w-[50%] h-full'>
-            container
+    return (
+        <div className='flex bg-gray-400 min-h-lvh lg:justify-center items-center gap-3'>
+            <div className='border border-black'>
+                <div className='bg-orange-300 w-[50%] h-full'>
+                    container
+                </div>
+                <ContactForm />
+            </div>
         </div>
-        <ContactForm />
-    </div>
-  )
+    )
 }
 
 export default ContactUs
