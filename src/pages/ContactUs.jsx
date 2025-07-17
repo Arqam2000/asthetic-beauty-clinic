2ded3b3a7cffa2f00a97fb43768276584eaa38dc
Arqam2000 <arqamsarwar9@gmail.com>
2025-07-17 12:15:14 +0500

    Update ContactUs.jsx

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
         <div className='flex lg:justify-center items-center gap-3 '>
             <div className=' w-full flex flex-col items-center gap-5 '>
                 <ContactForm />
-                <div className='bg-orange-300 lg:w-[66%] h-full'>
+                <div className=' lg:w-[66%] h-full'>
                     <div className='w-full'><iframe width="100%" height="400" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=karachi+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.mapsdirections.info/fr/calculer-la-population-sur-une-carte">calculer la population sur la carte</a></iframe></div>
                 </div>
             </div>
