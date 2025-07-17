ef0467c234e066313a067129cb8eff6c0211bb13
Arqam2000 <arqamsarwar9@gmail.com>
2025-07-17 15:03:34 +0500

    Update UI for ContactForm, AboutUs, Treatments, and navbar
    Improved the ContactForm heading and text, enhanced AboutUs and Treatments pages with styled containers and headings, and adjusted navbar styles for better alignment and layout on mobile.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactForm = () => {
     return (
         <div className='lg:w-[66%] h-full  flex flex-col items-center mt-4 bg-[#00000005]'>
-            <h1 className='text-2xl font-semibold my-2'>Contact Us</h1>
+            <h1 className='text-3xl font-semibold my-2'>Send us a message</h1>
             <form action="" className='flex flex-col gap-3 p-4 items-center sm:w-[100%] md:w-[60%] w-full '>
                 <input type="text" placeholder='Name' className='w-full outline p-1 px-3 bg-gray-100' />
                 <input type="email" placeholder='Email' className='w-full outline p-1 px-3 bg-gray-100' />
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>
-      About Us
+    <div className='h-[272px] text-center'>
+      <h1 className='text-2xl font-semibold'>About Us</h1>
     </div>
   )
 }
