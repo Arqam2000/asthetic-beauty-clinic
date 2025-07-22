02f412c856b5e1f697cf219b33166e4e367aacde
Arqam2000 <arqamsarwar9@gmail.com>
2025-07-22 14:35:47 +0500

    Add email and notification dependencies, update forms
    Added @emailjs/browser, @react-email/components, axios, nodemailer, and react-toastify to package.json for email and notification functionality. Updated ContactForm.jsx and BookAppointment.jsx to integrate email sending and toast notifications. Adjusted vite.config.js as needed for new dependencies.

--- src/pages/BookAppointment.jsx
+++ src/pages/BookAppointment.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
 const BookAppointment = () => {
+  const form = useRef();
+
+  const sendEmail = async (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('service_wy6ozsg', 'template_sd3r439', form.current, {
+        publicKey: 'zwlM7yzr_0kZNiU9Y',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          toast.success("Appointment booked successfully")
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          toast.error("Cannot booked the appointment")
+        },
+      );
+  }
+
   return (
     <>
       <h1 className='text-3xl font-semibold text-center my-3 mb-7'>Appointment</h1>
+      <ToastContainer />
       <div className='flex lg:gap-20 gap-4 flex-col md:flex-row justify-center items-center md:items-start  my-5'>
         <div className='flex flex-col justify-center items-center md:items-start gap-4'>
           <div>
@@ -16,13 +40,13 @@ const BookAppointment = () => {
           </div>
         </div>
         <div>
-          <form action="" className='flex flex-col gap-3'>
-            <input type="text" placeholder='First Name' className='outline p-1 px-3'/>
-            <input type="text" placeholder='Last Name' className='outline p-1 px-3'/>
-            <input type="email" placeholder='Email' className='outline p-1 px-3'/>
-            <input type="tel" placeholder='Phone' className='outline p-1 px-3'/>
-            <input type="datetime-local" name="" id="" className='outline p-1 px-3' />
-            <button className='bg-[#A64166] text-white rounded-full py-2 ' type='submit'>Book Appointment</button>
+          <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-3'>
+            <input type="text" placeholder='First Name' className='outline p-1 px-3' name='Fname'/>
+            <input type="text" placeholder='Last Name' className='outline p-1 px-3' name='Lname'/>
+            <input type="email" placeholder='Email' className='outline p-1 px-3' name='Email'/>
+            <input type="tel" placeholder='Phone' className='outline p-1 px-3' name='Phone'/>
+            <input type="datetime-local" name="time" id="" className='outline p-1 px-3' />
+            <button className='bg-[#A64166] text-white rounded-full py-2 cursor-pointer' type='submit'>Book Appointment</button>
           </form>
         </div>
       </div>
