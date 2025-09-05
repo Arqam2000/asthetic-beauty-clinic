57ec1234ba2700d88155d58bb7a5a4d96cbfab59
Arqam2000 <arqamsarwar9@gmail.com>
2025-09-05 15:17:37 +0500

    Add loading state to ContactForm submit button
    Introduces a loading state to the ContactForm component, updating the submit button text to indicate when the form is submitting. Also removes the 'required' attribute from input fields.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,15 +1,16 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 // import { sendEmail } from '../sendEmail/sendEmail';
 import axios from "axios"
 
 const ContactForm = () => {
+  const [loading, setLoading] = useState(false)
     const form = useRef();
 
   const sendMail = async (e) => {
     e.preventDefault();
-
+    setLoading(true)
     emailjs
       .sendForm('service_wy6ozsg', 'template_wmzrxae', form.current, {
         publicKey: 'zwlM7yzr_0kZNiU9Y',
@@ -24,6 +25,7 @@ const ContactForm = () => {
           toast.error("Cannot submit the form")
         },
       );
+    setLoading(false)
   }
 
     return (
@@ -31,11 +33,12 @@ const ContactForm = () => {
             <ToastContainer />
             <h1 className='text-3xl font-semibold my-2'>Send us a message</h1>
             <form ref={form} onSubmit={sendMail} className='flex flex-col gap-3 p-4 items-center sm:w-[100%] md:w-[60%] w-full '>
-                <input type="text" placeholder='Name' className='w-full outline p-1 px-3 bg-gray-100' name='Name' required/>
-                <input type="email" placeholder='Email' className='w-full outline p-1 px-3 bg-gray-100' name='Email' required/>
-                <input type="tel" placeholder='Phone' className='w-full outline p-1 px-3 bg-gray-100' required />
-                <textarea placeholder='Message' rows="4" cols="50" className='w-full outline p-1 px-3 bg-gray-100' name='Message' required></textarea>
-                <button type='submit' className='bg-[#A64166] py-1 px-3 rounded text-white cursor-pointer'>Submit</button>
+                <input type="text" placeholder='Name' className='w-full outline p-1 px-3 bg-gray-100' name='Name'/>
+                <input type="email" placeholder='Email' className='w-full outline p-1 px-3 bg-gray-100' name='Email'/>
+                <input type="tel" placeholder='Phone' className='w-full outline p-1 px-3 bg-gray-100' />
+                <textarea placeholder='Message' rows="4" cols="50" className='w-full outline p-1 px-3 bg-gray-100' name='Message'></textarea>
+                {/* <button type='submit' className='bg-[#A64166] py-1 px-3 rounded text-white cursor-pointer'>{loading? "Loading...": "Submit"}</button> */}
+                <input type="submit" value={loading? "Loading...": "Submit"} className='bg-[#A64166] py-1 px-3 rounded text-white cursor-pointer'/>
             </form>
         </div>
     )
